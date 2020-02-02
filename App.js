1b3ccbe4e0524d990b4df29cdd22bab8c7ce22e3
Shih-Wen Huang <wen@Shih-Wens-MacBook-Air.local>
2020-02-02 22:37:13 +0800

    load paymentscreen

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import PaymentScreen from './screens/PaymentScreen';
 
 import Footer from './components/Footer';
 
 export default function App() {
   return (
     <View style={styles.screen}>
-      
+        <PaymentScreen></PaymentScreen>
         <Footer />
      
     </View>
